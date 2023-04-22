d470cd4531d8083c9ff27d9379082223cbcb0ae4
martawto <118733839+martawto@users.noreply.github.com>
2023-04-22 08:38:31 +0000

    JAut iesakti

---
+++ main.py
@@ -0,0 +1,30 @@
+print("TESTS: Vai vari atbildēt pareizi uz 10 āķīgiem jautājumiem?") #vēlāk uzlikt treknāku tekstu, nomainīt krāsu moš aķīgs sarkanu
+
+jautajumi = ("Cik zirņi var ieiet vienā glāzē?: ", 
+"Kas ir smagāks - 1 kilograms kartupeļu vai 1 kilograms betona?: ", 
+"", 
+"", 
+"", 
+"", 
+"", 
+"", 
+"", 
+"")
+
+opcijas = (("A Pāris desmiti",  "B Pāris simti",  "C Viss atkarīgs no glāzes izmēra",  "D Necik"),
+("A Abi vienādi",  "B Kilograms kartupeļu",  "C Kilograms betona",  "D Kilograms biezpiena"),
+("",  "",  "",  ""),
+("",  "",  "",  ""),
+("",  "",  "",  ""),
+("",  "",  "",  ""),
+("",  "",  "",  ""),
+("",  "",  "",  ""),
+("",  "",  "",  ""),
+("",  "",  "",  ""),)
+
+atbildes = ()
+minejumi = []
+
+punkti = 0
+
+jautajumi_num = 0
