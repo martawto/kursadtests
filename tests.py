7b458568910a8e7722987d3e16fd4a385769ad4c
martawto <118733839+martawto@users.noreply.github.com>
2023-05-05 08:11:03 +0000

    jautajumi no txt, list ~

---
+++ tests.py
@@ -0,0 +1,51 @@
+# j = input("|")
+# a = 0
+
+# def jautajumi(j):
+#     f = open("jautajumi.txt", "r")
+#     a = int(input(f.read(97)) )
+#     print(a) 
+
+# jautajumi(j)
+
+# for i in range(10):
+#     jautajumi(j)
+
+
+# a = f.read(97)
+# print(a)
+# f.close()
+f = open("jautajumi.txt", "r")
+read = f.readlines()
+modif = []
+j = "|"
+
+for line in read:
+    if line[-1] == '\n' :
+        modif.append(line[: -1])
+    else:
+        modif.append(line)
+        
+
+print(modif)
+
+a = 0
+
+
+
+def jautajumi(j):
+    f = open("jautajumi.txt", "r")
+    a = int(input(f.readlines()) )
+
+    print(a) 
+
+count = 0
+
+for i in jautajumi(j):
+    count = count + 1
+    jautajumi(j)
+
+
+
+
+
